29fc3c98ef2b9631dc11bd22608369a6c93605a9
William Albright <ping@ewilli.am>
2013-08-19 11:01:30 -0700

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CaptionWatchApi::Application.config.secret_key_base = '6eb18461a75141877ef592cb979366eee50855e5814276e8137709524f9246e1b4a3454115236af1eebcba1bfafe3518f7a8b25eaaa405965397c0713d1249a7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CaptionWatchApi::Application.config.session_store :cookie_store, key: '_CaptionWatch-API_session'
